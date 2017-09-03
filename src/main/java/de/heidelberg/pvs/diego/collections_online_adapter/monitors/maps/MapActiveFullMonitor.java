7c95124791ad2184f19e3352338d4e4794ef5c2d
DiegoCosta <diegoelias1@gmail.com>
2017-09-03 23:43:39 +0200

    Support for the map active monitors

---
+++ src/main/java/de/heidelberg/pvs/diego/collections_online_adapter/monitors/maps/MapActiveFullMonitor.java
@@ -0,0 +1,79 @@
+package de.heidelberg.pvs.diego.collections_online_adapter.monitors.maps;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+
+public class MapActiveFullMonitor<K, V> implements Map<K, V> {
+	
+	private Map<K, V> map;
+	private MapState state;
+	
+	public MapActiveFullMonitor(Map<K, V> map, MapState state) {
+		super();
+		this.map = map;
+		this.state = state;
+	}
+	
+	public boolean containsKey(Object key) {
+		this.state.updateContainsOp(1);
+		return map.containsKey(key);
+	}
+
+	public boolean containsValue(Object value) {
+		this.state.updateIteration(1);
+		return map.containsValue(value);
+	}
+	
+	public V get(Object key) {
+		this.state.updateContainsOp(1);
+		return map.get(key);
+	}
+
+	public V put(K key, V value) {
+		this.state.updateSize(1);
+		return map.put(key, value);
+	}
+	
+	public int size() {
+		return map.size();
+	}
+
+	public boolean isEmpty() {
+		return map.isEmpty();
+	}
+
+	public V remove(Object key) {
+		return map.remove(key);
+	}
+
+	public void putAll(Map<? extends K, ? extends V> m) {
+		map.putAll(m);
+	}
+
+	public void clear() {
+		map.clear();
+	}
+
+	public Set<K> keySet() {
+		return map.keySet();
+	}
+
+	public Collection<V> values() {
+		return map.values();
+	}
+
+	public Set<java.util.Map.Entry<K, V>> entrySet() {
+		return map.entrySet();
+	}
+
+	public boolean equals(Object o) {
+		return map.equals(o);
+	}
+
+	public int hashCode() {
+		return map.hashCode();
+	}
+
+}
---
+++ src/main/java/de/heidelberg/pvs/diego/collections_online_adapter/monitors/maps/MapState.java
@@ -0,0 +1,46 @@
+package de.heidelberg.pvs.diego.collections_online_adapter.monitors.maps;
+
+import java.lang.ref.WeakReference;
+
+public class MapState {
+	
+	private int size;
+	private int containsOp;
+	private int iterationOp;
+	
+	private WeakReference<?> mapReference;
+	
+	public MapState(WeakReference<?> map) {
+		super();
+		this.mapReference = map;
+	}
+
+	public boolean hasCollectionFinished() {
+		return mapReference == null;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+
+	public int getContainsOp() {
+		return containsOp;
+	}
+
+	public int getIterationOp() {
+		return iterationOp;
+	}
+	
+	public void updateSize(int delta) {
+		size += delta;
+	}
+	
+	public void updateContainsOp(int delta) {
+		containsOp += delta;
+	}
+	
+	public void updateIteration(int delta) {
+		iterationOp += delta;
+	}
+
+}
